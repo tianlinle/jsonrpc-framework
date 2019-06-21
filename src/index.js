b1572e244e209dea5f70ee886f4f12a9708a3bf4
tian <tian@123.com>
2019-06-21 19:40:34 +0800

    expose JSON-RPC-client

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 module.exports = {
   Server: require('./Server'),
   ControllerBase: require('./ControllerBase'),
+  JsonRpcClient: require('./JsonRpcClient'),
+  JsonRpcClientError: require('./JsonRpcClientError'),
   ...require('tian-jsonrpc')
 };
